c1a7ce68109ded6f07781366d746a314fb9d6cb5
afiliushkin <cenyafilyushkin@gmail.com>
2025-10-16 18:39:12 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### My test parser
+
+For learning to upload crates for NaUKMA Rust course of 2025
+
+### Example
+
+```rust
+...
+```
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use ukma_practice2_2025_filiushkin::*;
+
+pub fn main() {
+    println!("{:?}", list_parser::list("[1,2,3,4,5,5]"));
+}
